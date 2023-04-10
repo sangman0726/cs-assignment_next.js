b57254af3f0b8dabc47ba99bfe9318e730691d35
sangman0726 <psmproaa@naver.com>
2023-04-10 23:16:16 +0900

    First Commit

---
+++ pages/posts/first-page.js
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+export default function FirstPage(){
+    return(
+        <div>
+            <ul>
+                <h2>
+                <h2><li>My name is 'SANGMAN PARK'.</li></h2>
+                <h2><li>I'm 24 years old.</li></h2>
+                <h2><li>My hobby is playing basektball.</li></h2>
+                <h2><li>My family has 6 members.</li></h2>
+                <h2><li>I have father, mother, two younger sisters, and a younger brother.</li></h2>
+                <h2><li>Thank you for watching!</li></h2>
+                <Link href="/">Back to home</Link>
+                </h2>
+            </ul>
+        </div>
+    )
+}
